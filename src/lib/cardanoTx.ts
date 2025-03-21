4d60132f4e24cf3ef0a906bc3a1763cd9134ba93
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-03-21 13:04:45 +0530

    minting tokens offchain

--- src/lib/cardanoTx.ts
+++ src/lib/cardanoTx.ts
@@ -1,6 +1,15 @@
 import { NFTMetadata } from "@/types/blockchain";
-import { Lucid } from "@lucid-evolution/lucid";
+import {
+  Data,
+  fromText,
+  Lucid,
+  mintingPolicyToId,
+  paymentCredentialOf,
+  validatorToAddress,
+} from "@lucid-evolution/lucid";
 import { NETWORK, PROVIDER } from "../config";
+import { AirNodeValidator } from "@/config/scripts/scripts";
+import { CampaignDatum } from "@/types/cardano";
 
 export async function mintNFTCardano(
   airNodeId: string,
@@ -11,6 +20,42 @@ export async function mintNFTCardano(
   try {
     const lucid = await Lucid(PROVIDER, NETWORK);
     lucid.selectWallet.fromAPI(walletApi);
+    const address = await lucid.wallet().address();
+
+    const validator = AirNodeValidator([paymentCredentialOf(address).hash]);
+    const contractAddress = validatorToAddress(NETWORK, validator);
+    const policyId = mintingPolicyToId(validator);
+
+    const tokens = {
+      [policyId + fromText(airNodeId)]: BigInt(fractionCount),
+    };
+
+    const redeemer: CampaignDatum = {
+      name: fromText(airNodeId),
+      goal: BigInt(fractionCount * 4_000_000),
+      fraction: BigInt(fractionCount),
+    };
+    const tx = await lucid
+      .newTx()
+      .mintAssets(tokens, Data.to(redeemer, CampaignDatum))
+      .pay.ToContract(
+        contractAddress,
+        { kind: "inline", value: Data.to(redeemer, CampaignDatum) },
+        { lovelace: 2_000_000n, ...tokens }
+      )
+      .attach.Script(validator)
+      .attachMetadata(721, {
+        [policyId]: {
+          [airNodeId]: {
+            name: airNodeId,
+            image: "https://avatars.githubusercontent.com/u/106166350",
+            ...metadata,
+          },
+        },
+      })
+      .addSigner(address)
+      .complete();
+    console.log("tx complete");
   } catch (error: any) {
     console.log(error);
   }
---
+++ src/types/cardano.ts
@@ -0,0 +1,104 @@
+import { Constr, Data, TLiteral, WalletApi } from "@lucid-evolution/lucid";
+
+//#region Alias
+export const PaymentKeyHashSchema = Data.Bytes();
+export const StakeKeyHashSchema = Data.Bytes();
+
+export const AddressSchema = Data.Tuple([
+  PaymentKeyHashSchema,
+  StakeKeyHashSchema,
+]);
+//#endregion
+
+//#region Enum
+
+export type CampaignState = "Initiated" | "Running" | "Cancelled" | "Finished";
+
+export const CampaignState: Record<
+  CampaignState,
+  { Title: CampaignState; Schema: TLiteral<CampaignState>; Constr: Constr<[]> }
+> = {
+  Initiated: {
+    Title: "Initiated",
+    Schema: Data.Literal("Initiated"),
+    Constr: new Constr(0, []),
+  },
+  Running: {
+    Title: "Running",
+    Schema: Data.Literal("Running"),
+    Constr: new Constr(1, []),
+  },
+  Cancelled: {
+    Title: "Cancelled",
+    Schema: Data.Literal("Cancelled"),
+    Constr: new Constr(2, []),
+  },
+  Finished: {
+    Title: "Finished",
+    Schema: Data.Literal("Finished"),
+    Constr: new Constr(3, []),
+  },
+};
+
+export const CampaignStateSchema = Data.Enum([
+  CampaignState.Initiated.Schema,
+  CampaignState.Running.Schema,
+  CampaignState.Cancelled.Schema,
+  CampaignState.Finished.Schema,
+]);
+
+export const CampaignStateRedeemer = {
+  Initiated: Data.to(new Constr(0, [])),
+  Running: Data.to(new Constr(1, [])),
+  Cancelled: Data.to(new Constr(2, [])),
+  Finished: Data.to(new Constr(3, [])),
+  Released: Data.to(new Constr(4, [])),
+};
+
+// export type CampaignState = Data.Static<typeof CampaignStateSchema>;
+// export const CampaignState = CampaignStateSchema as unknown as CampaignState;
+
+//#endregion
+
+//#region Datum
+
+export const CampaignDatumSchema = Data.Object({
+  name: Data.Bytes(),
+  goal: Data.Integer(),
+  fraction: Data.Integer(),
+});
+
+export type CampaignDatum = Data.Static<typeof CampaignDatumSchema>;
+export const CampaignDatum = CampaignDatumSchema as unknown as CampaignDatum;
+
+export const BackerDatumSchema = AddressSchema;
+export type BackerDatum = Data.Static<typeof BackerDatumSchema>;
+export const BackerDatum = BackerDatumSchema as unknown as BackerDatum;
+
+//-----------------------------
+export const MultisigSchema = Data.Object({
+  required: Data.Integer(),
+  signers: Data.Array(Data.Bytes()),
+});
+export type Multisig = Data.Static<typeof MultisigSchema>;
+export const Multisig = MultisigSchema as unknown as Multisig;
+//-----------------------------------
+
+export const ConfigDatumSchema = Data.Object({
+  multisig: MultisigSchema,
+  state_token_script: AddressSchema,
+  platform: Data.Bytes(),
+});
+
+export type ConfigDatum = Data.Static<typeof ConfigDatumSchema>;
+export const ConfigDatum = ConfigDatumSchema as unknown as ConfigDatum;
+//#endregion
+
+//#region Redeemer
+export const CampaignActionRedeemer = {
+  Support: Data.to(new Constr(0, [])),
+  Cancel: Data.to(new Constr(1, [])),
+  Finish: Data.to(new Constr(2, [])),
+  Refund: Data.to(new Constr(3, [])),
+  Release: Data.to(new Constr(4, [])),
+};
