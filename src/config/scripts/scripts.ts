1775170744d364b130eb95edb82e3100d94fcb0c
Cryptonean <faizanvit8@gmail.com>
2025-03-21 16:54:06 +0530

    pluuts and script

--- src/config/scripts/scripts.ts
+++ src/config/scripts/scripts.ts
@@ -1,9 +1,14 @@
 import {
   applyDoubleCborEncoding,
   applyParamsToScript,
+  MintingPolicy,
   Validator,
 } from "@lucid-evolution/lucid";
-import { placeholder_placeholder_spend } from "./plutus";
+import {
+  placeholder_placeholder_spend,
+  mint_token_placeholder_mint,
+} from "./plutus";
+import { mint } from "viem/chains";
 
 // ------------------------------------------------------------------
 const airnode_script = applyDoubleCborEncoding(placeholder_placeholder_spend);
@@ -14,3 +19,9 @@ export function AirNodeValidator(params: any[]): Validator {
     script: applyParamsToScript(airnode_script, params),
   };
 }
+
+const mint_token = applyDoubleCborEncoding(mint_token_placeholder_mint);
+export const mintingValidator: MintingPolicy = {
+  type: "PlutusV3",
+  script: mint_token_placeholder_mint,
+};
