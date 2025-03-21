45075030bcfca8167baa97e28b050a6f26b87da9
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-03-21 12:27:50 +0530

    aiken code

--- src/lib/cardanoTx.ts
+++ src/lib/cardanoTx.ts
@@ -9,7 +9,6 @@ export async function mintNFTCardano(
   walletApi: any
 ) {
   try {
-    console.log(metadata);
     const lucid = await Lucid(PROVIDER, NETWORK);
     lucid.selectWallet.fromAPI(walletApi);
   } catch (error: any) {
