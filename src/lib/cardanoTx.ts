1ea1e5e113f6d203e830e0579fcebf182e0828ea
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-04-01 11:54:49 +0530

    mint fraction

--- src/lib/cardanoTx.ts
+++ src/lib/cardanoTx.ts
@@ -31,71 +31,67 @@ export async function mintNFTCardano(
     //     const policyId = mintingPolicyToId(validator);
 
     const policyID = mintingPolicyToId(mintingValidator);
-    const assetName = "AirNode";
-    const fracToken = "Fraction-AirNode";
+    const policyId = policyID;
+    const contractAddress = validatorToAddress(NETWORK, mintingValidator);
+    //   const validator = AirNodeValidator([paymentCredentialOf(address).hash]);
+    //   const contractAddress = validatorToAddress(NETWORK, validator);
+    //   const policyId = mintingPolicyToId(validator);
 
-    const mintedAssets = { [`${policyID}${fromText(assetName)}`]: 1n };
-    const fractiontoken = {
-      [`${policyID}${fromText(fracToken)}`]: fractionCount,
+    const tokens = {
+      [policyId + fromText(airNodeId)]: fractionCount,
     };
     const redeemer = Data.void();
-    console.log("refder", redeemer);
-    console.log("pid", policyID);
 
     const tx = await lucid
       .newTx()
-      .mintAssets(mintedAssets, redeemer)
-      .mintAssets(fractiontoken, redeemer)
-
+      .mintAssets(tokens, redeemer)
+      .pay.ToContract(
+        contractAddress,
+        { kind: "inline", value: Data.void() },
+        { lovelace: 2_000_000n, ...tokens }
+      )
       .attach.MintingPolicy(mintingValidator)
+      .attachMetadata(721, {
+        [policyId]: {
+          [airNodeId]: {
+            name: airNodeId,
+            image: "https://avatars.githubusercontent.com/u/106166350",
+            // ...metadata,
+          },
+        },
+      })
+      .addSigner(address)
       .complete();
 
+    //   const redeemer: CampaignDatum = {
+    //     name: fromText(airNodeId),
+    //     goal: BigInt(fractionCount * 4_000_000),
+    //     fraction: BigInt(fractionCount),
+    //   };
+    //   const tx = await lucid
+    //     .newTx()
+    //     .mintAssets(tokens, Data.to(redeemer, CampaignDatum))
+    //     .pay.ToContract(
+    //       contractAddress,
+    //       { kind: "inline", value: Data.to(redeemer, CampaignDatum) },
+    //       { lovelace: 2_000_000n, ...tokens }
+    //     )
+    //     .attach.Script(validator)
+    //     .attachMetadata(721, {
+    //       [policyId]: {
+    //         [airNodeId]: {
+    //           name: airNodeId,
+    //           image: "https://avatars.githubusercontent.com/u/106166350",
+    //           ...metadata,
+    //         },
+    //       },
+    //     })
+    //     .addSigner(address)
+    // .complete();
     const signed = await tx.sign.withWallet().complete();
     const txHash = await signed.submit();
-    console.log("tx complete", txHash);
-  } catch (error) {
+    console.log(txHash);
+  } catch (error: any) {
     console.log(error);
   }
-  // try {
-  //   const lucid = await Lucid(PROVIDER, NETWORK);
-  //   lucid.selectWallet.fromAPI(walletApi);
-  //   const address = await lucid.wallet().address();
-
-  //   const validator = AirNodeValidator([paymentCredentialOf(address).hash]);
-  //   const contractAddress = validatorToAddress(NETWORK, validator);
-  //   const policyId = mintingPolicyToId(validator);
-
-  //   const tokens = {
-  //     [policyId + fromText(airNodeId)]: BigInt(fractionCount),
-  //   };
-
-  //   const redeemer: CampaignDatum = {
-  //     name: fromText(airNodeId),
-  //     goal: BigInt(fractionCount * 4_000_000),
-  //     fraction: BigInt(fractionCount),
-  //   };
-  //   const tx = await lucid
-  //     .newTx()
-  //     .mintAssets(tokens, Data.to(redeemer, CampaignDatum))
-  //     .pay.ToContract(
-  //       contractAddress,
-  //       { kind: "inline", value: Data.to(redeemer, CampaignDatum) },
-  //       { lovelace: 2_000_000n, ...tokens }
-  //     )
-  //     .attach.Script(validator)
-  //     .attachMetadata(721, {
-  //       [policyId]: {
-  //         [airNodeId]: {
-  //           name: airNodeId,
-  //           image: "https://avatars.githubusercontent.com/u/106166350",
-  //           ...metadata,
-  //         },
-  //       },
-  //     })
-  //     .addSigner(address)
-  //     .complete();
-  //   console.log("tx complete");
-  // } catch (error: any) {
-  //   console.log(error);
-  // }
 }
