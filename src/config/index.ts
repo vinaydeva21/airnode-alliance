a69c6265aecb98feb2b21f1d53972d0b33d99a3c
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-04-02 16:55:13 +0530

    OWNER

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,3 +6,5 @@ const NETWORKx = process.env.NEXT_PUBLIC_CARDANO_NETWORK as Network;
 
 export const NETWORK: Network = NETWORKx;
 export const PROVIDER: Provider = new Blockfrost(BF_URL, BF_PID);
+export const OWNER =
+  "addr_test1qrlq53qjd2yxx4lqj29526fn2uyl9fe7julp4shkgqm3m4dpvpz9h24n9ttq5f4d2xunltqy3yfphmr29uw4kwxt0h9qadh7tj";
